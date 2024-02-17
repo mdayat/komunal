5b36cbac48553a1a921a03bcc32094d2ffde6f97
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2024-02-17 21:06:12 +0700

    install and setup storybook and MUI

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-essentials"],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
